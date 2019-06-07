9261baaa9383c11328f625ce7d7e40405d77512a
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 16:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+    drivers.forEach(function (driver) {
+      console.log(driver['name']);
+    });
+};
+
+const logDriversByHometown = function (drivers, location) {
+    drivers.forEach(function (driver) {
+        if (driver['hometown'] == location){
+            console.log(driver['name']);
+        }
+    });
+};
+
+const driversByRevenue = function (drivers) {
+  let newDrivers = [...drivers];
+  newDrivers.sort(function(a,b){
+    return a['revenue'] - b['revenue'];
+    });
+    return newDrivers;
+};
